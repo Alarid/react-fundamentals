f71b2a7252dd8e593fe09a4b57922177dd87b066
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-04-07 20:50:38 +0200

    exercise 2

---
+++ src/exercise/02-perso.js
@@ -0,0 +1,43 @@
+/**
+ * IMPERATIVE PROGRAMMING
+ */
+
+function double(arr) {
+  let results = []
+  for (let i = 0; i < arr.length; i++) {
+    results.push(arr[i] * 2)
+  }
+  return results
+}
+function add(arr) {
+  let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i]
+  }
+  return result
+}
+
+$('#btn').click(function () {
+  $(this).toggleClass('highlight')
+  $(this).text() === 'Add Highlight'
+    ? $(this).text('Remove Highlight')
+    : $(this).text('Add Highlight')
+})
+
+/**
+ * DECLARATIVE PROGRAMING
+ */
+function double(arr) {
+  return arr.map(item => item * 2)
+}
+
+function add(arr) {
+  return arr.reduce((prev, current) => prev + current, 0)
+}
+
+;<Btn
+  onToggleHighlight={this.handleToggleHighlight}
+  highlight={this.state.highlight}
+>
+  {this.state.buttonText}
+</Btn>
